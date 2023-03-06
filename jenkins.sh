ebae35a3f4f115a82f7beb7d73488ca81d17dfc5
vinay <vinayj@gmail.com>
2023-03-06 15:09:19 +0000

    installation of jenkins

---
+++ jenkins.sh
@@ -0,0 +1,22 @@
+
+
+
+
+i
+
+
+
+#! /bin/bash
+
+   sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+  sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+   amazon-linux-extras install java-openjdk11-y 
+  
+  yum install jenkins -y  
+
+   service jenkins start
+   chkconfig jenkins on 
+
+
+

62cb2cc153f6fa849384f5db70060e2f6cf200d5
thehulkhunts <43322698+thehulkhunts@users.noreply.github.com>
2023-03-06 20:41:24 +0530

    update jenkins.sh
    adding some comments to Jenkins file

--- jenkins.sh
+++ jenkins.sh
@@ -1,17 +1,15 @@
 
-
-
-
-i
-
-
-
 #! /bin/bash
+############################
+# author: vinay
+# date: 06-03-2023
+# purpose: installation steps of jenkins
+####################################
 
-   sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+  sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
   sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 
-   amazon-linux-extras install java-openjdk11-y 
+  amazon-linux-extras install java-openjdk11-y 
   
   yum install jenkins -y  
 
